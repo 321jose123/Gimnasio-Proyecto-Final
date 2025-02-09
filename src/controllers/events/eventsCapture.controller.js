fa440c39462284c81792ae7db624ce81bc63b925
Kevin Santiago <kevinsansuor@gmail.com>
2025-02-09 02:48:39 -0500

    Documentación, minimizar objeto jsondata, filtro resultados con usuarios

--- src/controllers/events/eventsCapture.controller.js
+++ src/controllers/events/eventsCapture.controller.js
@@ -1,7 +1,6 @@
-const { HORARIO_EMPRESA_INICIO, HORARIO_EMPRESA_FIN } = require('../../../config');
+const { HORARIO_EMPRESA_INICIO, HORARIO_EMPRESA_FIN, API_URL_DEVICE_EVENTS } = require('../../../config');
 const { formatToUTC } = require('../../helpers/validate.helpers');
 const { apiService } = require('../../services/apiServices');
-const { API_URL_DEVICE_EVENTS } = require('../../../config');
 
 const { API_USERNAME, API_PASSWORD } = process.env;
 
@@ -11,23 +10,16 @@ const hora_final = `${new Date().toISOString().split('T')[0]}T${HORARIO_EMPRESA_
 const hora_inicio_utc = formatToUTC(hora_inicio);
 const hora_final_utc = formatToUTC(hora_final);
 
+/**
+ * Captura los eventos del dispositivo desde la hora de inicio del horario de la empresa hasta la hora final del horario de la empresa.
+ * @param {Object} req - Objeto de solicitud.
+ * @param {Object} res - Objeto de respuesta.
+ * @returns {Promise<Object>} - Objeto de respuesta con la lista de eventos capturados.
+ * @throws {Error} - Si hay un error al capturar eventos del dispositivo.
+ */
 const eventsCapture = async (req, res) => {
-    const fecha_inicio = hora_inicio
-    const fecha_final = hora_final
-
     try {
         const jsondata = {
-            AcsEventCond: {
-                searchID,
-                searchResultPosition,
-                maxResults,
-                major,
-                minor,
-                startTime,
-                endTime,
-                timeReverseOrder
-            }
-        } = {
             "AcsEventCond": {
                 "searchID": "prueba",
                 "searchResultPosition": 0,
@@ -40,38 +32,48 @@ const eventsCapture = async (req, res) => {
             }
         };
 
-        console.log(jsondata);
-        
+        console.log("Solicitud enviada a la API:", jsondata);
 
         let eventsUserCapture;
         try {
-            eventsUserCapture = await apiService.post(API_URL_DEVICE_EVENTS, API_USERNAME, API_PASSWORD, jsondata, "aplication/json");
+            eventsUserCapture = await apiService.post(API_URL_DEVICE_EVENTS, API_USERNAME, API_PASSWORD, jsondata, "application/json");
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ 
+            console.error("Error al capturar eventos del dispositivo:", error);
+            return res.status(500).json({ 
                 message: 'Error capturando evento en dispositivo',
                 status: 'error',
                 error: error.message,
             });
         }
 
-        res.status(200).json({ 
-            status: 'success',
-            message: 'Mostrando lista de eventos capturados',
-            data: eventsUserCapture,
-        });
+        if (eventsUserCapture && eventsUserCapture.AcsEvent && eventsUserCapture.AcsEvent.InfoList) {
+            const eventosFiltrados = eventsUserCapture.AcsEvent.InfoList.filter(evento => evento.employeeNoString);
+
+            return res.status(200).json({ 
+                status: 'success',
+                message: 'Mostrando lista de eventos capturados',
+                totalEventos: eventosFiltrados.length,
+                data: eventosFiltrados,
+            });
+        } else {
+            return res.status(200).json({
+                status: 'success',
+                message: 'No hay eventos registrados para el rango de tiempo solicitado.',
+                totalEventos: 0,
+                data: [],
+            });
+        }
+
     } catch (error) {
-        console.error(error);
+        console.error("Error en el proceso de captura:", error);
         res.status(500).json({ 
-            message: 'Error capturing event',
+            message: 'Error general en la captura de eventos',
             status: 'error',
             error: error.message,
         });
     }
-
-
 };
 
 module.exports = {
     eventsCapture,
-}
+};
